1d4c854a4e19babb36d8ca8b045296e4738a9968
kangyang <kangyang@putao.com>
2017-05-16 14:34:46 +0800

    update carthage build

---
+++ Carthage/Build/iOS/PTIMLibrary.framework/Headers/PTIM.h
@@ -0,0 +1,22 @@
+//
+//  PTIM.h
+//  PTIMLibrary
+//
+//  Created by admin on 2017/5/15.
+//  Copyright © 2017年 Putao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PTIMClient.h"
+
+@interface PTIM : NSObject
+
++ (PTIMConnectStatus)currentStatus;
+
++ (void)registerWithConfigure:(PTIMConfigure * _Nonnull)configure
+                     callback:(void (^_Nonnull)(BOOL successed))callback;
+
++ (void)sendCommand:(PTCommandMessage * _Nonnull)command
+           callback:(PTIMSendCommandCallback _Nonnull)callback;
+
+@end
