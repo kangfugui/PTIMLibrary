a9c7739c2eb74cbe237d5e209fd882a0cac25e39
kangyang <kangyang@putao.com>
2017-05-17 12:32:41 +0800

    example

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import PTIMLibrary
 
 class ViewController: UIViewController {
+    
+    let childrenID: Int32 = 10000
+    let appID: String = "1105"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +21,138 @@ class ViewController: UIViewController {
     
     @IBAction func lockButtonAction(_ sender: Any) {
         
-        let lock = PTLockCommand(state: true, lockdelay: 5)
-        let command = PTCommandContent(content: lock, type: .lock)
-        print(command)
+        var content = [String: Any]()
+        content["state"] = true
+        content["lockdelay"] = 5
+        
+        var value = [String: Any]()
+        value["appid"] = appID
+        value["type"] = "padlock"
+        value["session"] = Int(arc4random()%9999) + 1
+        value["content"] = content
+        
+        let data = try! JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+        let json = String(data: data, encoding: String.Encoding.utf8) ?? ""
+        
+        let message = PTCommandMessage()
+        message.content = json
+        message.toUsrID = childrenID
+        message.localID = String(format: "%.3f",NSDate().timeIntervalSince1970)
+
+        
+        PTIM.sendCommand(message) { success, result in
+            
+        }
+    }
+    
+    @IBAction func restButtonAction(_ sender: Any) {
+        
+        var content = [String: Any]()
+        content["state"] = true
+        content["availabletime"] = 20
+        content["resttime"] = 5
+        
+        var value = [String: Any]()
+        value["appid"] = appID
+        value["type"] = "timingrest"
+        value["session"] = Int(arc4random()%9999) + 1
+        value["content"] = content
+        
+        let data = try! JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+        let json = String(data: data, encoding: String.Encoding.utf8) ?? ""
+        
+        let message = PTCommandMessage()
+        message.content = json
+        message.toUsrID = childrenID
+        message.localID = String(format: "%.3f",NSDate().timeIntervalSince1970)
+        
+        
+        PTIM.sendCommand(message) { success, result in
+            
+        }
+    }
+    
+    @IBAction func sleepButtonAction(_ sender: Any) {
+        
+        var content = [String: Any]()
+        content["state"] = true
+        content["start"] = 1494979200
+        content["end"] = 1494993600
+        content["repeat"] = "1,2,3,4,5"
+        
+        var value = [String: Any]()
+        value["appid"] = appID
+        value["type"] = "sleep"
+        value["session"] = Int(arc4random()%9999) + 1
+        value["content"] = content
+        
+        let data = try! JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+        let json = String(data: data, encoding: String.Encoding.utf8) ?? ""
+        
+        let message = PTCommandMessage()
+        message.content = json
+        message.toUsrID = childrenID
+        message.localID = String(format: "%.3f",NSDate().timeIntervalSince1970)
+        
+        
+        PTIM.sendCommand(message) { success, result in
+            
+        }
+    }
+    
+    @IBAction func forbiddenButtonAction(_ sender: Any) {
+        
+        var content = [String: Any]()
+        content["state"] = true
+        content["start"] = 1494979200
+        content["end"] = 1494993600
+        content["repeat"] = "1,2,3,4,5"
+        content["package"] = ["all"]
+        
+        var value = [String: Any]()
+        value["appid"] = appID
+        value["type"] = "appTimeLimit"
+        value["session"] = Int(arc4random()%9999) + 1
+        value["content"] = content
+        
+        let data = try! JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+        let json = String(data: data, encoding: String.Encoding.utf8) ?? ""
+        
+        let message = PTCommandMessage()
+        message.content = json
+        message.toUsrID = childrenID
+        message.localID = String(format: "%.3f",NSDate().timeIntervalSince1970)
+        
+        
+        PTIM.sendCommand(message) { success, result in
+            
+        }
+    }
+    
+    @IBAction func limitButtonAction(_ sender: Any) {
+        
+        var content = [String: Any]()
+        content["time"] = 120
+        content["package"] = ""
+        
+        var value = [String: Any]()
+        value["appid"] = appID
+        value["type"] = "padTimeLimit"
+        value["session"] = Int(arc4random()%9999) + 1
+        value["content"] = content
+        
+        let data = try! JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+        let json = String(data: data, encoding: String.Encoding.utf8) ?? ""
+        
+        let message = PTCommandMessage()
+        message.content = json
+        message.toUsrID = childrenID
+        message.localID = String(format: "%.3f",NSDate().timeIntervalSince1970)
+        
+        
+        PTIM.sendCommand(message) { success, result in
+            
+        }
     }
 }
 
